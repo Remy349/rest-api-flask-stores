a4398e8feefb4eb1d3b1dc7c52e3c0f39c8bd1b6
Remy349 <santoraga15@gmail.com>
2023-07-26 00:39:11 -0600

    Add schemas design for the REST API.

--- resources/store.py
+++ resources/store.py
@@ -1,7 +1,7 @@
 import uuid
-from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from schemas import StoreSchema
 
 from db import stores
 
@@ -29,15 +29,8 @@ class StoreList(MethodView):
     def get(self):
         return {"stores": list(stores.values())}
 
-    def post(self):
-        store_data = request.get_json()
-
-        if "name" not in store_data:
-            abort(
-                400,
-                message="Bad request. Ensure 'name' is included in the JSON payload.",
-            )
-
+    @bp.arguments(StoreSchema)
+    def post(self, store_data):
         for store in stores.values():
             if store_data["name"] == store["name"]:
                 abort(400, message="Store already exists.")
